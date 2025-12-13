c6f0b2d7dcd81172b43b8a3b917e34c7b588040a
LorienWood <you@example.com>
2025-12-13 23:40:28 +0000

    commit

--- routes/exercise.js
+++ routes/exercise.js
@@ -124,8 +124,7 @@ router.post("/:workoutId/exercises/added", (req, res, next) => {
     (err) => {
       if (err) return next(err);
 
-      res.redirect(`/${workoutId}/exercises/add`);
-      
+      res.redirect(`/exercise/${workoutId}/exercises/add`);
     }
   );
 });
